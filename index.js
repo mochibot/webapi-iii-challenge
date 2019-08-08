480ec94f2bc3afcd2d2b8c432530c971b4973159
mochibot <amusaur@gmail.com>
2019-08-08 13:25:16 -0700

    sepcify to serve production ready react app and re-route client-side route to index.html

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 require('dotenv').config();
 const api = require('./server');
 
+if (process.env.NODE_ENV === "production") {
+  //serve any static files
+  api.use(express.static(path.join(__dirname, 'client/build')));
+  
+  //handle react routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 let port = process.env.PORT || 8000;
 api.listen(port, () => console.log(`listening on port ${port}`));
